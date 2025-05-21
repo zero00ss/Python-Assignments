c07fddafb96811eca86d1bee0ac3d9c6dd33cffc
zero00ss <singhsaksham2604@gmail.com>
2025-05-21 23:55:18 +0530

    moving

---
+++ ASSIGNMENT 1/Task 1.py
@@ -0,0 +1,13 @@
+a = input('Enter the first numb: ')
+a=float(a)
+b = input('Enter the second numb(cannot be zero): ')
+b=float(b)
+sum = a + b
+product = a * b
+division = a / b 
+difference = a - b
+
+print("Addition result: ", sum)
+print("Multiplication result: ", product)
+print("Division result: ", division)
+print("Subtraction result: ", difference)
---
+++ ASSIGNMENT 1/Task 2.py
@@ -0,0 +1,4 @@
+a= input("enter first name: ")
+b= input("enter second name: ")
+print("please check task 2 ",a+" "+b)
+print("some changes in task 2")
