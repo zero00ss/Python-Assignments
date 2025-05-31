b5893a1141fe2cfcee1abe184095cea62ef5647d
zero00ss <singhsaksham2604@gmail.com>
2025-05-31 17:12:49 +0530

    modified idk

--- ASSINGNMENT 5/dict.py
+++ ASSINGNMENT 5/dict.py
@@ -1,5 +1,7 @@
 std_dict={'Alice': 80, 'Bob': 90, 'Charlie': 85, 'David': 70, 'Eve': 95}
-a=input("Enter student name(case sensitive): ") 
+a=input("Enter student name: ")
+a=a.capitalize() 
+print(a)
 if a in std_dict:
     print("{}'s marks are {}".format(a, std_dict[a]))
 else:
